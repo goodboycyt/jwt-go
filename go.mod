e7f4dfa7c965a1a30287d91a2ad4313bc8787a1f
yatongchen <1836096205@qq.com>
2020-11-27 15:23:25 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/goodboycyt/jwt-go
+
+go 1.15
---
+++ jwt.go
@@ -0,0 +1,12 @@
+package jwt_go
+import "fmt"
+
+// Hello says hello.
+func Hello() {
+	fmt.Println("Hello go mod!")
+}
+
+// Bye says bye.
+func Bye() {
+	fmt.Println("Bye go mod!")
+}
