2cf7b26dfe29249ecdb784b2cd9f7e2150f9e76c
yatongchen <1836096205@qq.com>
2020-11-28 15:35:12 +0800

    完善

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# jwt-go
+## jwt go lib
+
+### use mod config
+`require github.com/goodboycyt/jwt-go latest`
+or
+`go get -u github.com/goodboycyt/jwt-go`
+
+
+### 生成token
+```go
+var jwtob jwt.Jwt
+jwtob.SetSecret("xDE}o4L1dVW+'@;P#=9]SFvVzPs'~Z")
+jwtob.SetExp(1700000000)
+jwtob.SetPublicPd(map[string]interface{}{"username":"sdsad", "userid":12121})
+a, err :=jwtob.GenSignature()
+```
+### 验证jwt字符串的真实性
+```go
+b, _ :=jwtob.VailDecSign(a)
+fmt.Println(b)//true or false
+```
+### 验证签名是否过期
+```go
+fmt.Println(jwtob.IsExp())//if had exp return,either return  false
+```
+
+### 获取payload信息
+```go
+//before do this,you must 'VailDecSign' the token
+fmt.Println(jwtob.RecPayload["username"])//RecPayload is a map,u can get fields with this var
+```
--- jwt.go
+++ jwt.go
@@ -1,9 +1,20 @@
 package jwt_go
 
 import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha256"
 	"encoding/base64"
-	"fmt"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"strings"
+	"time"
 )
+
+type JwtI interface {
+	SetPublicPd()
+}
 
 type Jwt struct {
 	secret string //密钥
@@ -15,41 +26,98 @@ type Jwt struct {
 	iat int64//jwt的签发时间
 	jti string//jwt的唯一身份标识，主要用来作为一次性token,从而回避重放攻击。
 
-	publicPayload []map[string]interface{}//公共信息
-	recPayload []map[string]interface{}//接收到的信息
+	publicPayload map[string]interface{}//公共信息
+	RecPayload map[string]interface{}//接收到的信息
 	//header string//头部信息
 }
 
-func (jwt Jwt) SetSecret(st string) {//设置密钥
+func (jwt *Jwt) SetSecret(st string) {//设置密钥
 	jwt.secret = st
 }
-func (jwt Jwt) SetIss(iss string) {//设置密钥
+func (jwt *Jwt) SetIss(iss string) {//设置密钥
 	jwt.iss = iss
 }
-func (jwt Jwt) SetSub(sub string) {//设置密钥
+func (jwt *Jwt) SetSub(sub string) {//设置密钥
 	jwt.sub = sub
 }
-func (jwt Jwt) SetAud(aud string) {//设置密钥
+func (jwt *Jwt) SetAud(aud string) {//设置密钥
 	jwt.aud = aud
 }
-func (jwt Jwt) SetExp(exp int64) {//设置密钥
+func (jwt *Jwt) SetExp(exp int64) {//设置密钥
 	jwt.exp = exp
 }
-func (jwt Jwt) SetNbf(nbf int64) {//设置密钥
+func (jwt *Jwt) SetNbf(nbf int64) {//设置密钥
 	jwt.nbf = nbf
 }
-func (jwt Jwt) SetIat(iat int64) {//设置密钥
+func (jwt *Jwt) SetIat(iat int64) {//设置密钥
 	jwt.iat = iat
 }
-func (jwt Jwt) SetJti(jti string) {//设置密钥
+func (jwt *Jwt) SetJti(jti string) {//设置密钥
 	jwt.jti = jti
 }
-func (jwt Jwt) SetPublicPd(publicPayload []map[string]interface{}) {//设置密钥
+func (jwt *Jwt) SetPublicPd(publicPayload map[string]interface{}) {//设置密钥
 	jwt.publicPayload = publicPayload
 }
 
-func (jwt Jwt) GenSignature() {//生成jwt字符串
+func (jwt *Jwt) GenSignature() (string,error){//生成jwt字符串
+	if jwt.secret=="" {
+		return "",errors.New("secret must not be empty!")
+	}
 	header := base64.StdEncoding.EncodeToString([]byte("{\"typ\":\"JWT\",\"alg\":\"HS256\"}"))//头部
 	payload := map[string]interface{}{"iss":jwt.iss, "sub":jwt.sub,"aud":jwt.aud,"exp":jwt.exp,"nbf":jwt.nbf,"iat":jwt.iat,"jti":jwt.jti}
-	fmt.Println(header,payload)
+	for k ,v := range jwt.publicPayload {
+		payload[k] = v
+	}
+	pyJson,_ :=json.Marshal(payload)
+	tmp := base64.StdEncoding.EncodeToString(pyJson)
+	return header+"."+tmp+"."+hmacSha256(header+md5T(tmp)+tmp, jwt.secret),nil
+	//return "wew"
+
+}
+
+func (jwt *Jwt) VailDecSign(token string) (bool,error){//生成jwt字符串
+	tokenArr :=strings.Split(token,".")
+	if jwt.secret=="" || len(tokenArr)!=3{
+		return false,errors.New("secret must not be empty!.token struct error")
+	}
+	if hmacSha256(tokenArr[0]+md5T(tokenArr[1])+tokenArr[1], jwt.secret) != tokenArr[2] {
+		return false,errors.New("token is illegal")
+	}
+	payload, err0 := base64.StdEncoding.DecodeString(tokenArr[1])
+	if err0!=nil {
+		return false,errors.New("payload base64 decode error")
+	}
+	var payMap map[string]interface{}
+	if err := json.Unmarshal([]byte(payload), &payMap); err != nil {
+		return false,errors.New("payload json struct error")
+	}
+	jwt.RecPayload = payMap
+
+	return true,nil
+
+
+}
+
+func hmacSha256(data string, secret string) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(data))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func md5T(str string) string {
+	m5 := md5.New()
+	_,err := m5.Write([]byte(str))
+	if err != nil {
+		panic(err)
+	}
+	md5String := hex.EncodeToString(m5.Sum(nil))
+	return md5String
+}
+
+func (jwt *Jwt) IsExp() bool{
+	if int64(jwt.RecPayload["exp"].(float64))<time.Now().Unix() {
+		return true
+	} else {
+		return false
+	}
 }
